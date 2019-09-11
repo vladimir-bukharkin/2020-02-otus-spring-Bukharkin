0743661affb0fc8d52dd210b327c10b21d35c7b9
vbukharkin <bukharkinv@gmail.com>
2019-09-11 23:43:17 +0300

    Adds questions.path property

--- HW01-spring-ioc-introduce/src/main/java/ru/otus/hw01/dao/QuestionCsvDao.java
+++ HW01-spring-ioc-introduce/src/main/java/ru/otus/hw01/dao/QuestionCsvDao.java
@@ -3,6 +3,8 @@ package ru.otus.hw01.dao;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw01.domain.question.Question;
 import ru.otus.hw01.exception.IOModuleException;
@@ -21,14 +23,15 @@ import java.util.List;
 import java.util.Objects;
 
 @Repository
+@PropertySource("classpath:application.properties")
 public class QuestionCsvDao implements QuestionDao {
 
     private final Path csvPath;
     private final QuestionValidator questionValidator;
 
-    public QuestionCsvDao(QuestionValidator questionValidator) { //todo path
+    public QuestionCsvDao(@Value("${questions.path}") String csvPath, QuestionValidator questionValidator) {
         try {
-            this.csvPath = Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource("questions.csv")).toURI());
+            this.csvPath = Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource(csvPath)).toURI());
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }

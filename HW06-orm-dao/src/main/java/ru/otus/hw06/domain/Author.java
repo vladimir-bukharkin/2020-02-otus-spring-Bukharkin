495504968fcba0304b20b7d910c4477894ae820b
Vladimir Bukharkin <v.bukharkin@infomaximum.com>
2020-03-16 14:44:41 +0300

    Inits hw06 module

---
+++ HW06-orm-dao/src/main/java/ru/otus/hw06/domain/Author.java
@@ -0,0 +1,17 @@
+package ru.otus.hw06.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Author {
+
+    @Setter
+    private Long id;
+    private final String firstName;
+    private final String lastName;
+}

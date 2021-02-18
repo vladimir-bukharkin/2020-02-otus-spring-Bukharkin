75089aabbc2f35c2b0b425a186b95fa2a67c35f2
vbukharkin <bukharkinv@gmail.com>
2021-02-19 01:40:05 +0300

    Adds restfull controller

--- HW10-react/src/main/java/ru/otus/hw10/controller/BookController.java
+++ HW10-react/src/main/java/ru/otus/hw10/controller/BookController.java
@@ -1,33 +1,63 @@
 package ru.otus.hw10.controller;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.otus.hw10.dto.Book;
-import ru.otus.hw10.exception.DomainObjectNotFoundException;
+import ru.otus.hw10.exception.BookNotFoundException;
 import ru.otus.hw10.service.BookService;
 
-import java.util.List;
 
 @RestController
 public class BookController {
 
     private final BookService bookService;
+    private final BookModelAssembler assembler;
 
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, BookModelAssembler assembler) {
         this.bookService = bookService;
+        this.assembler = assembler;
     }
 
     @GetMapping("/book")
-    public List<Book> all() {
-        return bookService.findAll();
+    public CollectionModel<EntityModel<Book>> all() {
+        return assembler.toCollectionModel(bookService.findAll());
+    }
+
+    @PostMapping("/book")
+    public ResponseEntity<?> create(@RequestBody Book book) {
+        var createdBook = assembler.toModel(bookService.save(book));
+
+        return ResponseEntity.created(createdBook.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(createdBook);
+    }
+
+    @DeleteMapping("/book/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        bookService.deleteById(id);
+
+        return ResponseEntity.noContent()
+                .build();
+    }
+
+    @PutMapping("/book/{id}")
+    public ResponseEntity<?> update(@RequestBody Book book, @PathVariable String id) {
+        book.setId(id);
+        var updatedBook = assembler.toModel(bookService.save(book));
+
+        return ResponseEntity.created(updatedBook.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(updatedBook);
     }
 
     @GetMapping("/book/{id}")
-    public Book get(@PathVariable String id) throws DomainObjectNotFoundException {
-        return bookService.findById(id).orElseThrow(() -> new DomainObjectNotFoundException("Book", id));
+    public EntityModel<Book> get(@PathVariable String id) throws BookNotFoundException {
+        Book result = bookService.findById(id)
+                .orElseThrow(() -> new BookNotFoundException(id));
+
+        return assembler.toModel(result);
     }
 
     @GetMapping("/edit_book")
--- HW10-react/src/main/java/ru/otus/hw10/exception/CoreException.java
+++ HW10-react/src/main/java/ru/otus/hw10/exception/CoreException.java
@@ -1,6 +1,6 @@
 package ru.otus.hw10.exception;
 
-public class CoreException extends Exception {
+public class CoreException extends RuntimeException {
 
     public CoreException(String message) {
         super(message);
